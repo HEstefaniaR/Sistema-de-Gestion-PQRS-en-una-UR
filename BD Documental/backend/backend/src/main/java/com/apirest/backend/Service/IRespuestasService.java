a2bd55583e983699547f421db0ea47a2acb01654
MRAMCG <162338868+MRAMCG@users.noreply.github.com>
2025-05-21 11:11:35 -0500

    Create IRespuestasService.java

---
+++ BD Documental/backend/backend/src/main/java/com/apirest/backend/Service/IRespuestasService.java
@@ -0,0 +1,13 @@
+package com.apirest.backend.Service;
+
+import java.util.List;
+import org.bson.types.ObjectId;
+import com.apirest.backend.Model.RespuestaModel;
+
+public interface IRespuestaService {
+    String guardarRespuesta(RespuestaModel respuesta);
+    List<RespuestaModel> listarRespuestas();
+    RespuestaModel buscarRespuestaPorId(ObjectId id);
+    RespuestaModel actualizarRespuesta(ObjectId id, RespuestaModel respuesta);
+    void eliminarRespuesta(ObjectId id);
+}
