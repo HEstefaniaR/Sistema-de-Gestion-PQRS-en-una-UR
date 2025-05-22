a2642280e12459d882384d96673451e59614597c
MRAMCG <162338868+MRAMCG@users.noreply.github.com>
2025-05-22 14:57:31 -0500

    Update IRespuestaRepository.java

--- BD Documental/backend/backend/src/main/java/com/apirest/backend/Repository/IRespuestaRepository.java
+++ BD Documental/backend/backend/src/main/java/com/apirest/backend/Repository/IRespuestaRepository.java
@@ -1,10 +1,9 @@
 package com.apirest.backend.Repository;
 
-import java.util.List;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import com.apirest.backend.Model.RespuestaModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.bson.types.ObjectId;
+import java.util.List;
 
 public interface IRespuestaRepository extends MongoRepository<RespuestaModel, ObjectId> {
     List<RespuestaModel> findBySolicitudId(ObjectId solicitudId);
